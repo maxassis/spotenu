14a0ce51e6c35155f0129e9a1861546d59eee45a
maxwelb-assis <max.assis@ymail.com>
2020-08-25 19:10:38 -0300

    terminado tela de login

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import GlobalStyles from './components/styles/global';
 import Inicio from './components/Inicio';
 import SignupUsuario from './components/signup/SignUsuario/index';
 import SelecionarUsuario from './components/selecionarUsuario';
+import Login from './components/login/index'
 
 function App() {
   return (
     <>
       <GlobalStyles/>
      { /* <Inicio/> */} 
-      <SignupUsuario/> 
+     {/* <SignupUsuario/> */} 
      {/* <SelecionarUsuario/> */}
+     <Login/>
 
     </>
   );
---
+++ src/components/login/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import Header from '../header/index'
+import Footer from '../footer/index'
+import * as S from './styled'
+import { useForm } from "react-hook-form";
+
+
+function Login() {
+    const { register, errors, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
+    return(
+        <>
+         <Header/>
+        <S.Container>
+        <S.ContSignup>
+            <S.Divi>
+            <S.Texto>Login</S.Texto>
+            </S.Divi>
+
+            <S.Formulario onSubmit={handleSubmit(onSubmit)}>
+            
+            <S.Leibel> Email </S.Leibel>
+            <S.Inputi placeholder="Digite seu email" name="email" ref={register({ required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/ })} />
+            {errors.email && "Digite um email valido"}
+
+            <S.Leibel> Senha </S.Leibel>
+            <S.Inputi label="password" placeholder="Digite uma senha" name="password" type="password" ref={register({ required: true, minLength: 8})} />
+            {errors.password && "Digite uma senha com no minimo 8 caracteres"}
+
+            <S.Botao type="submit">Enviar</S.Botao> 
+
+            <S.Inscrevase>Nao possui inscrição ? <a href="/">Inscreva-se</a></S.Inscrevase>
+
+            </S.Formulario>
+
+    
+        </S.ContSignup> 
+
+        </S.Container>
+
+        <Footer/>
+
+        
+        </>
+
+
+
+
+    )
+
+
+
+
+
+}
+
+export default Login
